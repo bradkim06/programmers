10f00441279f414f88015a7f51dad76677291632
bradkim06 <bradkim06@gmail.com>
2021-10-21 17:27:44 +0900

    실패율

---
+++ Lv1/실패율/Solution.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        int[] countStageUser = new int[N];
+
+        Map<Integer, Float> map = new HashMap<>();
+
+        for (int i = 0; i < stages.length; i++) {
+            if (stages[i] == N + 1) {
+                continue;
+            }
+            countStageUser[stages[i] - 1]++;
+        }
+
+        int numStageUser = stages.length;
+        for (int i = 0; i < N; i++) {
+            if (numStageUser == 0) {
+                map.put(i, 0f);
+                continue;
+            }
+
+            map.put(i, (float) countStageUser[i] / (float) numStageUser);
+            numStageUser -= countStageUser[i];
+        }
+
+        List<Integer> keySetList = new ArrayList<>(map.keySet());
+
+        Collections.sort(keySetList, (o1, o2) -> (map.get(o2).compareTo(map.get(o1))));
+        int index = 0;
+        for (Integer key : keySetList) {
+            answer[index++] = key + 1;
+        }
+
+        return answer;
+    }
+
+    public static void main(String... args) {
+        Solution test = new Solution();
+
+        int[] stages = { 2, 1, 2, 6, 2, 4, 3, 3 };
+        System.out.println(Arrays.toString(test.solution(5, stages)));
+    }
+}
