545ba7f46fc5fe76e813bbac97867f621ab357dc
bradkim06 <bradkim06@gmail.com>
2021-10-22 13:37:52 +0900

    타겟넘버

---
+++ Lv2/타겟넘버/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        answer = dfs(numbers, 0, 0, target);
+        return answer;
+    }
+
+    private int dfs(int[] numbers, int node, int sum, int target) {
+        if (node == numbers.length) {
+            if (sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+
+        return dfs(numbers, node + 1, sum + numbers[node], target)
+                + dfs(numbers, node + 1, sum - numbers[node], target);
+    }
+
+    public static void main(String... args) {
+        Solution test = new Solution();
+
+        int[] numbers = { 1, 1, 1, 1, 1 };
+        System.out.println(test.solution(numbers, 3));
+    }
+}
